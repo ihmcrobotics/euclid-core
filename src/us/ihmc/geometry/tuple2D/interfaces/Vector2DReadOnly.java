292f5644e67dd14871e5142c0bf705ee170c9a2f
Sylvain Bertrand <sbertrand@ihmc.us>
2017-02-07 18:26:37 -0600

    Documented Vector2DReadOnly.

--- src/us/ihmc/geometry/tuple2D/interfaces/Vector2DReadOnly.java
+++ src/us/ihmc/geometry/tuple2D/interfaces/Vector2DReadOnly.java
@@ -1,22 +1,84 @@
 package us.ihmc.geometry.tuple2D.interfaces;
 
+/**
+ * Read-only interface for a 2 dimensional vector.
+ * <p>
+ * A 2D vector represents a physical quantity with a magnitude and a direction in the XY-plane.
+ * For instance, it can be used to represent a 2D velocity, force, or translation from one 2D point to another.
+ * </p>
+ * <p>
+ * Although a point and vector hold onto the same type of information, the distinction is made between them
+ * as they represent different geometry objects and are typically not handled the same way:
+ * <ul>
+ *    <li> a point represents the coordinate of a location in space.
+ *     A notable difference with a vector is that the distance between two points has a physical meaning.
+ *     When a point is transformed with a homogeneous transformation matrix,
+ *     a point's coordinates are susceptible to be scaled, rotated, and translated.
+ *    <li> a vector is not constrained to a location in space. Instead, a vector represents some
+ *     physical quantity that has a direction and a magnitude such as: a velocity, a force, the translation from one
+ *     point to another, etc.
+ *     When a vector is transformed with a homogeneous transformation matrix,
+ *     its components are susceptible to be scaled and rotated, but never to be translated.
+ * </ul> 
+ * </p>
+ * 
+ * @author Sylvain Bertrand
+ *
+ * @param <T> The final type of the vector used.
+ */
 public interface Vector2DReadOnly<T extends Vector2DReadOnly<T>> extends Tuple2DReadOnly<T>
 {
+   /**
+    * Calculates and returns the magnitude of this vector.
+    * 
+    * @return the magnitude of this vector.
+    */
    default double length()
    {
       return Math.sqrt(lengthSquared());
    }
 
+   /**
+    * Calculates and returns the square of the magnitude of this vector.
+    * <p>
+    * This method is usually preferred over {@link #length()}
+    * when calculation speed matters and knowledge of the actual magnitude does not,
+    * i.e. when comparing several vectors by theirs magnitude.
+    * </p>
+    * 
+    * @return the square of the magnitude of this vector.
+    */
    default double lengthSquared()
    {
       return dot(this);
    }
 
+   /**
+    * Calculates and returns the value of the dot product of this vector with {@code other}.
+    * 
+    * @param other the other vector used for the dot product. Not modified.
+    * @return the value of the dot product.
+    */
    default double dot(Vector2DReadOnly<?> other)
    {
       return getX() * other.getX() + getY() * other.getY();
    }
 
+   /**
+    * Calculates and returns the angle in radians from this vector to {@code other}.
+    * <p>
+    * The computed angle is in the range [-<i>pi</i>; <i>pi</i>].
+    * </p>
+    * <p>
+    * Edge cases:
+    * <ul>
+    *    <li> if the length of either vector is below {@code 1.0E-7}, this method fails and returns an angle of {@code 0.0} radian.
+    * </ul>
+    * </p>
+    * 
+    * @param other the other vector used to compute the angle. Not modified.
+    * @return the value of the angle from this vector to {@code other}.
+    */
    default double angle(Vector2DReadOnly<?> other)
    {
       double firstVectorX = getX();
@@ -53,13 +115,26 @@ public interface Vector2DReadOnly<T extends Vector2DReadOnly<T>> extends Tuple2D
       return angle;
    }
 
+   /**
+    * Calculates and returns the value of the cross product of this vector with {@code other}.
+    * 
+    * @param other the second vector of the cross product. Not modified.
+    * @return the value of the cross product.
+    */
    default double cross(Vector2DReadOnly<?> other)
    {
       return cross(this, other);
    }
 
-   public static double cross(Vector2DReadOnly<?> v1, Vector2DReadOnly<?> v2)
+   /**
+    * Calculates and returns the value of the cross product of {@code vector1} with {@code vector2}.
+    * 
+    * @param vector1 the first vector in the cross product. Not modified.
+    * @param vector2 the second vector in the cross product. Not modified.
+    * @return the value of the cross product.
+    */
+   public static double cross(Vector2DReadOnly<?> vector1, Vector2DReadOnly<?> vector2)
    {
-      return v1.getX() * v2.getY() - v1.getY() * v2.getX();
+      return vector1.getX() * vector2.getY() - vector1.getY() * vector2.getX();
    }
 }
