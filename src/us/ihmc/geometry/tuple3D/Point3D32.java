467e70ea302527b8ab27e93c0a02fd01bac9aaaa
Sylvain Bertrand <sbertrand@ihmc.us>
2017-02-08 08:23:54 -0600

    Deleted Tuple3D32.

--- src/us/ihmc/geometry/tuple3D/Point3D32.java
+++ src/us/ihmc/geometry/tuple3D/Point3D32.java
@@ -5,27 +5,126 @@ import java.io.Serializable;
 import us.ihmc.geometry.tuple3D.interfaces.Point3DBasics;
 import us.ihmc.geometry.tuple3D.interfaces.Tuple3DReadOnly;
 
-public class Point3D32 extends Tuple3D32<Point3D32> implements Serializable, Point3DBasics<Point3D32>
+public class Point3D32 implements Serializable, Point3DBasics<Point3D32>
 {
    private static final long serialVersionUID = 5142616577127976269L;
 
+   private float x;
+   private float y;
+   private float z;
+
    public Point3D32()
    {
-      super();
+      setToZero();
    }
 
    public Point3D32(float x, float y, float z)
    {
-      super(x, y, z);
+      set(x, y, z);
    }
 
    public Point3D32(float[] pointArray)
    {
-      super(pointArray);
+      set(pointArray);
    }
 
    public Point3D32(Tuple3DReadOnly<?> other)
    {
-      super(other);
+      set(other);
+   }
+
+   @Override
+   public void setX(double x)
+   {
+      this.x = (float) x;
+   }
+
+   @Override
+   public void setY(double y)
+   {
+      this.y = (float) y;
+   }
+
+   @Override
+   public void setZ(double z)
+   {
+      this.z = (float) z;
+   }
+
+   public void setX(float x)
+   {
+      this.x = x;
+   }
+
+   public void setY(float y)
+   {
+      this.y = y;
+   }
+
+   public void setZ(float z)
+   {
+      this.z = z;
+   }
+
+   @Override
+   public double getX()
+   {
+      return x;
+   }
+
+   @Override
+   public double getY()
+   {
+      return y;
+   }
+
+   @Override
+   public double getZ()
+   {
+      return z;
+   }
+
+   public float getX32()
+   {
+      return x;
+   }
+
+   public float getY32()
+   {
+      return y;
+   }
+
+   public float getZ32()
+   {
+      return z;
+   }
+
+   @Override
+   public boolean equals(Object object)
+   {
+      try
+      {
+         return equals((Point3D32) object);
+      }
+      catch (ClassCastException e)
+      {
+         return false;
+      }
+   }
+
+   @Override
+   public String toString()
+   {
+      return "(" + x + ", " + y + ", " + z + ")";
+   }
+
+   @Override
+   public int hashCode()
+   {
+      long bits = 1L;
+      bits = 31L * bits + Float.floatToIntBits(x);
+      bits = 31L * bits + Float.floatToIntBits(y);
+      bits = 31L * bits + Float.floatToIntBits(z);
+      return (int) (bits ^ bits >> 32);
    }
 }
--- src/us/ihmc/geometry/tuple3D/Vector3D32.java
+++ src/us/ihmc/geometry/tuple3D/Vector3D32.java
@@ -2,29 +2,129 @@ package us.ihmc.geometry.tuple3D;
 
 import java.io.Serializable;
 
+import us.ihmc.geometry.tuple3D.interfaces.Tuple3DReadOnly;
 import us.ihmc.geometry.tuple3D.interfaces.Vector3DBasics;
 
-public class Vector3D32 extends Tuple3D32<Vector3D32> implements Serializable, Vector3DBasics<Vector3D32>
+public class Vector3D32 implements Serializable, Vector3DBasics<Vector3D32>
 {
    private static final long serialVersionUID = 1186918378545386628L;
 
+   private float x;
+   private float y;
+   private float z;
+
    public Vector3D32()
    {
-      super();
+      setToZero();
    }
 
    public Vector3D32(float x, float y, float z)
    {
-      super(x, y, z);
+      set(x, y, z);
    }
 
    public Vector3D32(float[] vectorArray)
    {
-      super(vectorArray);
+      set(vectorArray);
    }
 
-   public Vector3D32(Tuple3D32<?> tuple)
+   public Vector3D32(Tuple3DReadOnly<?> tuple)
    {
-      super(tuple);
+      set(tuple);
+   }
+
+   @Override
+   public void setX(double x)
+   {
+      this.x = (float) x;
+   }
+
+   @Override
+   public void setY(double y)
+   {
+      this.y = (float) y;
+   }
+
+   @Override
+   public void setZ(double z)
+   {
+      this.z = (float) z;
+   }
+
+   public void setX(float x)
+   {
+      this.x = x;
+   }
+
+   public void setY(float y)
+   {
+      this.y = y;
+   }
+
+   public void setZ(float z)
+   {
+      this.z = z;
+   }
+
+   @Override
+   public double getX()
+   {
+      return x;
+   }
+
+   @Override
+   public double getY()
+   {
+      return y;
+   }
+
+   @Override
+   public double getZ()
+   {
+      return z;
+   }
+
+   public float getX32()
+   {
+      return x;
+   }
+
+   public float getY32()
+   {
+      return y;
+   }
+
+   public float getZ32()
+   {
+      return z;
+   }
+
+   @Override
+   public boolean equals(Object object)
+   {
+      try
+      {
+         return equals((Vector3D32) object);
+      }
+      catch (ClassCastException e)
+      {
+         return false;
+      }
+   }
+
+   @Override
+   public String toString()
+   {
+      return "(" + x + ", " + y + ", " + z + ")";
+   }
+
+   @Override
+   public int hashCode()
+   {
+      long bits = 1L;
+      bits = 31L * bits + Float.floatToIntBits(x);
+      bits = 31L * bits + Float.floatToIntBits(y);
+      bits = 31L * bits + Float.floatToIntBits(z);
+      return (int) (bits ^ bits >> 32);
    }
 }
