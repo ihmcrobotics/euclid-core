0832f8cb9d40dfeeff75e1066cdb159ceef91e79
Sylvain Bertrand <sbertrand@ihmc.us>
2017-02-08 08:29:55 -0600

    Documented Tuple3DReadOnly.

--- src/us/ihmc/geometry/tuple3D/interfaces/Tuple3DReadOnly.java
+++ src/us/ihmc/geometry/tuple3D/interfaces/Tuple3DReadOnly.java
@@ -5,34 +5,106 @@ import org.ejml.data.DenseMatrix64F;
 import us.ihmc.geometry.interfaces.EpsilonComparable;
 import us.ihmc.geometry.tuple3D.Tuple3DTools;
 
+/**
+ * Read-only interface for a 3 dimensional tuple.
+ * <p>
+ * A tuple is an abstract geometry object holding onto the common math between a 3D point and vector.
+ * </p>
+ * <p>
+ * Although a point and vector hold onto the same type of information, the distinction is made between them
+ * as they represent different geometry objects and are typically not handled the same way:
+ * <ul>
+ *    <li> a point represents the coordinate of a location in space.
+ *     A notable difference with a vector is that the distance between two points has a physical meaning.
+ *     When a point is transformed with a homogeneous transformation matrix,
+ *     a point's coordinates are susceptible to be scaled, rotated, and translated.
+ *    <li> a vector is not constrained to a location in space. Instead, a vector represents some
+ *     physical quantity that has a direction and a magnitude such as: a velocity, a force, the translation from one
+ *     point to another, etc.
+ *     When a vector is transformed with a homogeneous transformation matrix,
+ *     its components are susceptible to be scaled and rotated, but never to be translated.
+ * </ul> 
+ * </p>
+ * 
+ * @author Sylvain Bertrand
+ *
+ * @param <T> The final type of the tuple used.
+ */
 public interface Tuple3DReadOnly<T extends Tuple3DReadOnly<T>> extends EpsilonComparable<T>
 {
+   /**
+    * Returns the x-component of this tuple.
+    * 
+    * @return the x-component.
+    */
    double getX();
 
+   /**
+    * Returns the y-component of this tuple.
+    * 
+    * @return the y-component.
+    */
    double getY();
 
+   /**
+    * Returns the z-component of this tuple.
+    * 
+    * @return the z-component.
+    */
    double getZ();
 
+   /**
+    * Returns the x-component of this tuple.
+    * 
+    * @return the x-component.
+    */
    default float getX32()
    {
       return (float) getX();
    }
 
+   /**
+    * Returns the y-component of this tuple.
+    * 
+    * @return the y-component.
+    */
    default float getY32()
    {
       return (float) getY();
    }
 
+   /**
+    * Returns the z-component of this tuple.
+    * 
+    * @return the z-component.
+    */
    default float getZ32()
    {
       return (float) getZ();
    }
 
+   /**
+    * Tests if this tuple contains a {@link Double#NaN}.
+    * 
+    * @return {@code true} if this tuple contains a {@link Double#NaN}, {@code false} otherwise.
+    */
    default boolean containsNaN()
    {
       return Double.isNaN(getX()) || Double.isNaN(getY()) || Double.isNaN(getZ());
    }
 
+   /**
+    * Selects a component of this tuple based on {@code index}
+    * and returns its value.
+    * <p>
+    * For an {@code index} of 0, the corresponding component is {@code x}, 1
+    * it is {@code y}, 2 it is {@code z}.
+    * </p>
+    * 
+    * @param index the index of the component to get.
+    * @return the value of the component.
+    * @throws IndexOutOfBoundsException if {@code index} &notin; [0, 2].
+    */
    default double get(int index)
    {
       switch (index)
@@ -48,6 +120,18 @@ public interface Tuple3DReadOnly<T extends Tuple3DReadOnly<T>> extends EpsilonCo
       }
    }
 
+   /**
+    * Selects a component of this tuple based on {@code index}
+    * and returns its value.
+    * <p>
+    * For an {@code index} of 0, the corresponding component is {@code x}, 1
+    * it is {@code y}, 2 it is {@code z}.
+    * </p>
+    * 
+    * @param index the index of the component to get.
+    * @return the value of the component.
+    * @throws IndexOutOfBoundsException if {@code index} &notin; [0, 2].
+    */
    default double get32(int index)
    {
       switch (index)
@@ -63,11 +147,22 @@ public interface Tuple3DReadOnly<T extends Tuple3DReadOnly<T>> extends EpsilonCo
       }
    }
 
+   /**
+    * Packs the components of this tuple in an array starting from its first index.
+    * 
+    * @param tupleArrayToPack the array in which this tuple is stored. Modified.
+    */
    default void get(double[] tupleArrayToPack)
    {
       get(0, tupleArrayToPack);
    }
 
+   /**
+    * Packs the components of this tuple in an array starting from {@code startIndex}.
+    *
+    * @param startIndex the index in the array where the first component is stored.
+    * @param tupleArrayToPack the array in which this tuple is stored. Modified.
+    */
    default void get(int startIndex, double[] tupleArrayToPack)
    {
       tupleArrayToPack[startIndex++] = getX();
@@ -75,11 +170,22 @@ public interface Tuple3DReadOnly<T extends Tuple3DReadOnly<T>> extends EpsilonCo
       tupleArrayToPack[startIndex] = getZ();
    }
 
+   /**
+    * Packs the components of this tuple in an array starting from its first index.
+    * 
+    * @param tupleArrayToPack the array in which this tuple is stored. Modified.
+    */
    default void get(float[] tupleArrayToPack)
    {
       get(0, tupleArrayToPack);
    }
 
+   /**
+    * Packs the components of this tuple in an array starting from {@code startIndex}.
+    *
+    * @param startIndex the index in the array where the first component is stored.
+    * @param tupleArrayToPack the array in which this tuple is stored. Modified.
+    */
    default void get(int startIndex, float[] tupleArrayToPack)
    {
       tupleArrayToPack[startIndex++] = getX32();
@@ -87,16 +193,34 @@ public interface Tuple3DReadOnly<T extends Tuple3DReadOnly<T>> extends EpsilonCo
       tupleArrayToPack[startIndex] = getZ32();
    }
 
+   /**
+    * Packs the components of this tuple in a column vector starting from its first row index.
+    * 
+    * @param tupleMatrixToPack the array in which this tuple is stored. Modified.
+    */
    default void get(DenseMatrix64F tupleMatrixToPack)
    {
       get(0, 0, tupleMatrixToPack);
    }
 
+   /**
+    * Packs the components of this tuple in a column vector starting from {@code startRow}.
+    * 
+    * @param startRow the first row index to start writing in the dense-matrix.
+    * @param tupleMatrixToPack the column vector in which this tuple is stored. Modified.
+    */
    default void get(int startRow, DenseMatrix64F tupleMatrixToPack)
    {
       get(startRow, 0, tupleMatrixToPack);
    }
 
+   /**
+    * Packs the components of this tuple in a matrix starting from {@code startRow} at the column index {@code column}.
+    * 
+    * @param startRow the first row index to start writing in the dense-matrix.
+    * @param column the column index to write in the dense-matrix.
+    * @param tupleMatrixToPack the matrix in which this tuple is stored. Modified.
+    */
    default void get(int startRow, int column, DenseMatrix64F tupleMatrixToPack)
    {
       tupleMatrixToPack.set(startRow++, column, getX());
@@ -104,12 +228,25 @@ public interface Tuple3DReadOnly<T extends Tuple3DReadOnly<T>> extends EpsilonCo
       tupleMatrixToPack.set(startRow, column, getZ());
    }
 
+   /**
+    * Tests on a per component basis if this tuple is equal to the given {@code other} to an {@code epsilon}.
+    * 
+    * @param other the other tuple to compare against this. Not modified.
+    * @param epsilon the tolerance to use when comparing each component.
+    * @return {@code true} if the two tuples are equal, {@code false} otherwise.
+    */
    @Override
    default boolean epsilonEquals(T other, double epsilon)
    {
       return Tuple3DTools.epsilonEquals(this, other, epsilon);
    }
 
+   /**
+    * Tests on a per component basis, if this tuple is exactly equal to {@code other}.
+    * 
+    * @param other the other tuple to compare against this. Not modified.
+    * @return {@code true} if the two tuples are exactly equal component-wise, {@code false} otherwise.
+    */
    default boolean equals(T other)
    {
       try
