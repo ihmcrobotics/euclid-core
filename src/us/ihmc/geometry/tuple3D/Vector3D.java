3ac63c62282a08190a5db7faa0ea1e72d08d5f43
Sylvain Bertrand <sbertrand@ihmc.us>
2017-02-08 08:58:37 -0600

    Documented Vector3D and Vector3D32.

--- src/us/ihmc/geometry/tuple3D/Vector3D.java
+++ src/us/ihmc/geometry/tuple3D/Vector3D.java
@@ -5,70 +5,141 @@ import java.io.Serializable;
 import us.ihmc.geometry.tuple3D.interfaces.Tuple3DReadOnly;
 import us.ihmc.geometry.tuple3D.interfaces.Vector3DBasics;
 
+/**
+ * A 3D vector represents a physical quantity with a magnitude and a direction.
+ * For instance, it can be used to represent a 3D velocity, force, or translation from one 3D point to another.
+ * <p>
+ * This version of 3D vector uses double precision fields to save the value of each component.
+ * It is meant for garbage free usage.
+ * </p>
+ * 
+ * @author Sylvain Bertrand
+ */
 public class Vector3D implements Serializable, Vector3DBasics<Vector3D>
 {
    private static final long serialVersionUID = -8494204802892437237L;
 
+   /** The x-component. */
    private double x;
+   /** The y-component. */
    private double y;
+   /** The z-component. */
    private double z;
 
+   /**
+    * Creates a new vector and initializes it components to zero.
+    */
    public Vector3D()
    {
       setToZero();
    }
 
+   /**
+    * Creates a new vector and initializes it with the given components.
+    * 
+    * @param x the x-component.
+    * @param y the y-component.
+    * @param z the z-component.
+    */
    public Vector3D(double x, double y, double z)
    {
       set(x, y, z);
    }
 
+   /**
+    * Creates a new vector and initializes it from the given array.
+    * 
+    * @param pointArray the array containing this vector's components. Not modified.
+    */
    public Vector3D(double[] vectorArray)
    {
       set(vectorArray);
    }
 
+   /**
+    * Creates a new vector and initializes it to {@code other}
+    * 
+    * @param other the tuple to copy the components from. Not modified.
+    */
    public Vector3D(Tuple3DReadOnly<?> other)
    {
       set(other);
    }
 
+   /**
+    * Sets the x-component of this vector.
+    * 
+    * @param x the x-component.
+    */
    @Override
    public void setX(double x)
    {
       this.x = x;
    }
 
+   /**
+    * Sets the y-component of this vector.
+    * 
+    * @param y the y-component.
+    */
    @Override
    public void setY(double y)
    {
       this.y = y;
    }
 
+   /**
+    * Sets the z-component of this vector.
+    * 
+    * @param z the z-component.
+    */
    @Override
    public void setZ(double z)
    {
       this.z = z;
    }
 
+   /**
+    * Returns the value of the x-component of this vector.
+    * 
+    * @return the x-component's value.
+    */
    @Override
    public double getX()
    {
       return x;
    }
 
+   /**
+    * Returns the value of the y-component of this vector.
+    * 
+    * @return the y-component's value.
+    */
    @Override
    public double getY()
    {
       return y;
    }
 
+   /**
+    * Returns the value of the z-component of this vector.
+    * 
+    * @return the z-component's value.
+    */
    @Override
    public double getZ()
    {
       return z;
    }
 
+   /**
+    * Tests if the given {@code object}'s class is the same as this,
+    * in which case the method returns {@link #equals(Vector3D)}, it returns {@code false}
+    * otherwise.
+    * 
+    * @param object the object to compare against this. Not modified.
+    * @return {@code true} if {@code object} and this are exactly equal, {@code false} otherwise.
+    */
    @Override
    public boolean equals(Object object)
    {
@@ -82,12 +153,24 @@ public class Vector3D implements Serializable, Vector3DBasics<Vector3D>
       }
    }
 
+   /**
+    * Provides a {@code String} representation of this vector 3D as follows:
+    * (x, y, z).
+    * 
+    * @return the {@code String} representing this vector 3D.
+    */
    @Override
    public String toString()
    {
       return "(" + x + ", " + y + ", " + z + ")";
    }
 
+   /**
+    * Calculates and returns a hash code value from the value
+    * of each component of this vector 3D.
+    * 
+    * @return the hash code value for this vector 3D.
+    */
    @Override
    public int hashCode()
    {
