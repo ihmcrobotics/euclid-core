182bb2017b7bc4f85d1edb7da9ec7e3cb5792d18
Sylvain Bertrand <sbertrand@ihmc.us>
2017-02-08 08:53:17 -0600

    Documented Point3D and Point3D32.

--- src/us/ihmc/geometry/tuple3D/Point3D.java
+++ src/us/ihmc/geometry/tuple3D/Point3D.java
@@ -5,70 +5,140 @@ import java.io.Serializable;
 import us.ihmc.geometry.tuple3D.interfaces.Point3DBasics;
 import us.ihmc.geometry.tuple3D.interfaces.Tuple3DReadOnly;
 
+/**
+ * A 3D point represents the 3D coordinates of a location in space.
+ * <p>
+ * This version of 3D point uses double precision fields to save the value of each component.
+ * It is meant for garbage free usage.
+ * </p>
+ * 
+ * @author Sylvain Bertrand
+ */
 public class Point3D implements Serializable, Point3DBasics<Point3D>
 {
    private static final long serialVersionUID = -1234830724104344277L;
 
+   /** The x-coordinate. */
    private double x;
+   /** The y-coordinate. */
    private double y;
+   /** The z-coordinate. */
    private double z;
 
+   /**
+    * Creates a new point and initializes it coordinates to zero.
+    */
    public Point3D()
    {
       setToZero();
    }
 
+   /**
+    * Creates a new point and initializes it with the given coordinates.
+    * 
+    * @param x the x-coordinate.
+    * @param y the y-coordinate.
+    * @param z the z-coordinate.
+    */
    public Point3D(double x, double y, double z)
    {
       set(x, y, z);
    }
 
+   /**
+    * Creates a new point and initializes it from the given array.
+    * 
+    * @param pointArray the array containing this point's coordinates. Not modified.
+    */
    public Point3D(double[] pointArray)
    {
       set(pointArray);
    }
 
+   /**
+    * Creates a new point and initializes it to {@code other}
+    * 
+    * @param other the tuple to copy the coordinates from. Not modified.
+    */
    public Point3D(Tuple3DReadOnly<?> other)
    {
       set(other);
    }
 
+   /**
+    * Sets the x-coordinate of this point.
+    * 
+    * @param x the x-coordinate.
+    */
    @Override
    public void setX(double x)
    {
       this.x = x;
    }
 
+   /**
+    * Sets the y-coordinate of this point.
+    * 
+    * @param y the y-coordinate.
+    */
    @Override
    public void setY(double y)
    {
       this.y = y;
    }
 
+   /**
+    * Sets the z-coordinate of this point.
+    * 
+    * @param z the z-coordinate.
+    */
    @Override
    public void setZ(double z)
    {
       this.z = z;
    }
 
+   /**
+    * Returns the value of the x-coordinate of this point.
+    * 
+    * @return the x-coordinate's value.
+    */
    @Override
    public double getX()
    {
       return x;
    }
 
+   /**
+    * Returns the value of the y-coordinate of this point.
+    * 
+    * @return the y-coordinate's value.
+    */
    @Override
    public double getY()
    {
       return y;
    }
 
+   /**
+    * Returns the value of the z-coordinate of this point.
+    * 
+    * @return the z-coordinate's value.
+    */
    @Override
    public double getZ()
    {
       return z;
    }
 
+   /**
+    * Tests if the given {@code object}'s class is the same as this,
+    * in which case the method returns {@link #equals(Point3D)}, it returns {@code false}
+    * otherwise.
+    * 
+    * @param object the object to compare against this. Not modified.
+    * @return {@code true} if {@code object} and this are exactly equal, {@code false} otherwise.
+    */
    @Override
    public boolean equals(Object object)
    {
@@ -82,12 +152,24 @@ public class Point3D implements Serializable, Point3DBasics<Point3D>
       }
    }
 
+   /**
+    * Provides a {@code String} representation of this point 3D as follows:
+    * (x, y, z).
+    * 
+    * @return the {@code String} representing this point 3D.
+    */
    @Override
    public String toString()
    {
       return "(" + x + ", " + y + ", " + z + ")";
    }
 
+   /**
+    * Calculates and returns a hash code value from the value
+    * of each component of this point 3D.
+    * 
+    * @return the hash code value for this point 3D.
+    */
    @Override
    public int hashCode()
    {
--- src/us/ihmc/geometry/tuple3D/Point3D32.java
+++ src/us/ihmc/geometry/tuple3D/Point3D32.java
@@ -5,100 +5,204 @@ import java.io.Serializable;
 import us.ihmc.geometry.tuple3D.interfaces.Point3DBasics;
 import us.ihmc.geometry.tuple3D.interfaces.Tuple3DReadOnly;
 
+/**
+ * A 3D point represents the 3D coordinates of a location in space.
+ * <p>
+ * This version of 3D point uses single precision fields to save the value of each component.
+ * It is meant for garbage free usage and for situations where heap memory is limited.
+ * When memory is not a constraint, the use of {@link Point3D} is preferable.
+ * </p>
+ * 
+ * @author Sylvain Bertrand
+ */
 public class Point3D32 implements Serializable, Point3DBasics<Point3D32>
 {
    private static final long serialVersionUID = 5142616577127976269L;
 
+   /** The x-coordinate. */
    private float x;
+   /** The y-coordinate. */
    private float y;
+   /** The z-coordinate. */
    private float z;
 
+   /**
+    * Creates a new point and initializes it coordinates to zero.
+    */
    public Point3D32()
    {
       setToZero();
    }
 
+   /**
+    * Creates a new point and initializes it with the given coordinates.
+    * 
+    * @param x the x-coordinate.
+    * @param y the y-coordinate.
+    * @param z the z-coordinate.
+    */
    public Point3D32(float x, float y, float z)
    {
       set(x, y, z);
    }
 
+   /**
+    * Creates a new point and initializes it from the given array.
+    * 
+    * @param pointArray the array containing this point's coordinates. Not modified.
+    */
    public Point3D32(float[] pointArray)
    {
       set(pointArray);
    }
 
+   /**
+    * Creates a new point and initializes it to {@code other}
+    * 
+    * @param other the tuple to copy the coordinates from. Not modified.
+    */
    public Point3D32(Tuple3DReadOnly<?> other)
    {
       set(other);
    }
 
+   /**
+    * Sets the x-coordinate of this point.
+    * 
+    * @param x the x-coordinate.
+    */
    @Override
    public void setX(double x)
    {
       this.x = (float) x;
    }
 
+   /**
+    * Sets the y-coordinate of this point.
+    * 
+    * @param y the y-coordinate.
+    */
    @Override
    public void setY(double y)
    {
       this.y = (float) y;
    }
 
+   /**
+    * Sets the z-coordinate of this point.
+    * 
+    * @param z the z-coordinate.
+    */
    @Override
    public void setZ(double z)
    {
       this.z = (float) z;
    }
 
+   /**
+    * Sets the x-coordinate of this point.
+    * 
+    * @param x the x-coordinate.
+    */
    public void setX(float x)
    {
       this.x = x;
    }
 
+   /**
+    * Sets the y-coordinate of this point.
+    * 
+    * @param y the y-coordinate.
+    */
    public void setY(float y)
    {
       this.y = y;
    }
 
+   /**
+    * Sets the z-coordinate of this point.
+    * 
+    * @param z the z-coordinate.
+    */
    public void setZ(float z)
    {
       this.z = z;
    }
 
+   /**
+    * Returns the value of the x-coordinate of this point.
+    * 
+    * @return the x-coordinate's value.
+    */
    @Override
    public double getX()
    {
       return x;
    }
 
+   /**
+    * Returns the value of the y-coordinate of this point.
+    * 
+    * @return the y-coordinate's value.
+    */
    @Override
    public double getY()
    {
       return y;
    }
 
+   /**
+    * Returns the value of the z-coordinate of this point.
+    * 
+    * @return the z-coordinate's value.
+    */
    @Override
    public double getZ()
    {
       return z;
    }
 
+   /**
+    * Returns the value of the x-coordinate of this point.
+    * 
+    * @return the x-coordinate's value.
+    */
+   @Override
    public float getX32()
    {
       return x;
    }
 
+   /**
+    * Returns the value of the y-coordinate of this point.
+    * 
+    * @return the y-coordinate's value.
+    */
+   @Override
    public float getY32()
    {
       return y;
    }
 
+   /**
+    * Returns the value of the z-coordinate of this point.
+    * 
+    * @return the z-coordinate's value.
+    */
+   @Override
    public float getZ32()
    {
       return z;
    }
 
+   /**
+    * Tests if the given {@code object}'s class is the same as this,
+    * in which case the method returns {@link #equals(Point3D32)}, it returns {@code false}
+    * otherwise.
+    * 
+    * @param object the object to compare against this. Not modified.
+    * @return {@code true} if {@code object} and this are exactly equal, {@code false} otherwise.
+    */
    @Override
    public boolean equals(Object object)
    {
@@ -112,12 +216,24 @@ public class Point3D32 implements Serializable, Point3DBasics<Point3D32>
       }
    }
 
+   /**
+    * Provides a {@code String} representation of this point 3D as follows:
+    * (x, y, z).
+    * 
+    * @return the {@code String} representing this point 3D.
+    */
    @Override
    public String toString()
    {
       return "(" + x + ", " + y + ", " + z + ")";
    }
 
+   /**
+    * Calculates and returns a hash code value from the value
+    * of each component of this point 3D.
+    * 
+    * @return the hash code value for this point 3D.
+    */
    @Override
    public int hashCode()
    {
