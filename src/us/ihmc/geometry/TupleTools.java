f1a2b8700bf16598c16e8d754b1ef7376b7cd838
Sylvain Bertrand <sbertrand@ihmc.us>
2017-02-09 10:41:01 -0600

    Documented and added tests to TupleTools.

--- src/us/ihmc/geometry/TupleTools.java
+++ src/us/ihmc/geometry/TupleTools.java
@@ -6,7 +6,14 @@ import us.ihmc.geometry.tuple4D.interfaces.Tuple4DReadOnly;
 
 public abstract class TupleTools
 {
-   
+   /**
+    * Tests on a per component basis if the two given tuples are equal to an {@code epsilon}.
+    * 
+    * @param tuple1 the first tuple. Not modified.
+    * @param tuple2 the second tuple. Not modified.
+    * @param epsilon the tolerance to use when comparing each component.
+    * @return {@code true} if the two tuples are equal, {@code false} otherwise.
+    */
    public static boolean epsilonEquals(Tuple2DReadOnly<?> tuple1, Tuple2DReadOnly<?> tuple2, double epsilon)
    {
       double difference;
@@ -22,6 +29,14 @@ public abstract class TupleTools
       return true;
    }
 
+   /**
+    * Tests on a per component basis if the two given tuples are equal to an {@code epsilon}.
+    * 
+    * @param tuple1 the first tuple. Not modified.
+    * @param tuple2 the second tuple. Not modified.
+    * @param epsilon the tolerance to use when comparing each component.
+    * @return {@code true} if the two tuples are equal, {@code false} otherwise.
+    */
    public static boolean epsilonEquals(Tuple3DReadOnly<?> tuple1, Tuple3DReadOnly<?> tuple2, double epsilon)
    {
       double difference;
@@ -41,6 +56,14 @@ public abstract class TupleTools
       return true;
    }
 
+   /**
+    * Tests on a per component basis if the two given tuples are equal to an {@code epsilon}.
+    * 
+    * @param tuple1 the first tuple. Not modified.
+    * @param tuple2 the second tuple. Not modified.
+    * @param epsilon the tolerance to use when comparing each component.
+    * @return {@code true} if the two tuples are equal, {@code false} otherwise.
+    */
    public static boolean epsilonEquals(Tuple4DReadOnly<?> tuple1, Tuple4DReadOnly<?> tuple2, double epsilon)
    {
       double difference;
@@ -64,6 +87,19 @@ public abstract class TupleTools
       return true;
    }
 
+   /**
+    * Performs a linear interpolation from {@code a} to {@code b} given the 
+    * percentage {@code alpha}.
+    * <p>
+    * result = (1.0 - alpha) * a + alpha * b
+    * </p>
+    * 
+    * @param a the first value used in the interpolation.
+    * @param b the second value used in the interpolation.
+    * @param alpha the percentage to use for the interpolation.
+    * A value of 0 will return {@code a}, while a value of 1 will return {@code b}.
+    * @return the interpolated value.
+    */
    public static double interpolate(double a, double b, double alpha)
    {
       return (1.0 - alpha) * a + alpha * b;
--- test/us/ihmc/geometry/TupleToolsTest.java
+++ test/us/ihmc/geometry/TupleToolsTest.java
@@ -8,18 +8,64 @@ import java.util.Random;
 
 import org.junit.Test;
 
-import us.ihmc.geometry.TupleTools;
 import us.ihmc.geometry.tuple2D.Point2D;
 import us.ihmc.geometry.tuple2D.interfaces.Tuple2DBasics;
 import us.ihmc.geometry.tuple3D.Point3D;
 import us.ihmc.geometry.tuple3D.interfaces.Tuple3DBasics;
+import us.ihmc.geometry.tuple4D.Vector4D;
+import us.ihmc.geometry.tuple4D.interfaces.Vector4DBasics;
 
 public class TupleToolsTest
 {
    private static final int NUMBER_OF_ITERATIONS = 100;
 
    @Test
-   public void testEpsilonEqualsTuple() throws Exception
+   public void testEpsilonEqualsTuple2D() throws Exception
+   {
+      Random random = new Random(621541L);
+      Tuple2DBasics<?> tuple1 = new Point2D();
+      Tuple2DBasics<?> tuple2 = new Point2D();
+
+      double epsilon = random.nextDouble();
+
+      tuple1.setX(random.nextDouble());
+      tuple1.setY(random.nextDouble());
+
+      tuple2.setX(tuple1.getX() + 0.1 * epsilon);
+      tuple2.setY(tuple1.getY() + 0.1 * epsilon);
+      assertTrue(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+
+      tuple2.setX(tuple1.getX() - 0.1 * epsilon);
+      tuple2.setY(tuple1.getY() - 0.1 * epsilon);
+      assertTrue(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+
+      tuple2.setX(tuple1.getX() - 1.1 * epsilon);
+      tuple2.setY(tuple1.getY() - 0.1 * epsilon);
+      assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+
+      tuple2.setX(tuple1.getX() - 0.1 * epsilon);
+      tuple2.setY(tuple1.getY() - 1.1 * epsilon);
+      assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+
+      tuple2.setX(tuple1.getX() + 1.1 * epsilon);
+      tuple2.setY(tuple1.getY() + 0.1 * epsilon);
+      assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+
+      tuple2.setX(tuple1.getX() + 0.1 * epsilon);
+      tuple2.setY(tuple1.getY() + 1.1 * epsilon);
+      assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+
+      tuple2.setX(tuple1.getX() + epsilon);
+      tuple2.setY(tuple1.getY() + epsilon);
+      assertTrue(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+
+      tuple2.setX(tuple1.getX() - epsilon);
+      tuple2.setY(tuple1.getY() - epsilon);
+      assertTrue(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+   }
+
+   @Test
+   public void testEpsilonEqualsTuple3D() throws Exception
    {
       Random random = new Random(621541L);
       Tuple3DBasics<?> tuple1 = new Point3D();
@@ -80,51 +126,92 @@ public class TupleToolsTest
       tuple2.setY(tuple1.getY() - epsilon);
       tuple2.setZ(tuple1.getZ() - epsilon);
       assertTrue(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
-
    }
 
    @Test
-   public void testEpsilonEqualsTuple2D() throws Exception
+   public void testEpsilonEqualsTuple4D() throws Exception
    {
       Random random = new Random(621541L);
-      Tuple2DBasics<?> tuple1 = new Point2D();
-      Tuple2DBasics<?> tuple2 = new Point2D();
+      Vector4DBasics<?> tuple1 = new Vector4D();
+      Vector4DBasics<?> tuple2 = new Vector4D();
 
       double epsilon = random.nextDouble();
 
       tuple1.setX(random.nextDouble());
       tuple1.setY(random.nextDouble());
+      tuple1.setZ(random.nextDouble());
+      tuple1.setS(random.nextDouble());
 
       tuple2.setX(tuple1.getX() + 0.1 * epsilon);
       tuple2.setY(tuple1.getY() + 0.1 * epsilon);
+      tuple2.setZ(tuple1.getZ() + 0.1 * epsilon);
+      tuple2.setS(tuple1.getS() + 0.1 * epsilon);
       assertTrue(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
 
       tuple2.setX(tuple1.getX() - 0.1 * epsilon);
       tuple2.setY(tuple1.getY() - 0.1 * epsilon);
+      tuple2.setZ(tuple1.getZ() - 0.1 * epsilon);
+      tuple2.setS(tuple1.getS() - 0.1 * epsilon);
       assertTrue(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
 
       tuple2.setX(tuple1.getX() - 1.1 * epsilon);
       tuple2.setY(tuple1.getY() - 0.1 * epsilon);
+      tuple2.setZ(tuple1.getZ() - 0.1 * epsilon);
+      tuple2.setS(tuple1.getS() - 0.1 * epsilon);
       assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
 
       tuple2.setX(tuple1.getX() - 0.1 * epsilon);
       tuple2.setY(tuple1.getY() - 1.1 * epsilon);
+      tuple2.setZ(tuple1.getZ() - 0.1 * epsilon);
+      tuple2.setS(tuple1.getS() - 0.1 * epsilon);
+      assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+
+      tuple2.setX(tuple1.getX() - 0.1 * epsilon);
+      tuple2.setY(tuple1.getY() - 0.1 * epsilon);
+      tuple2.setZ(tuple1.getZ() - 1.1 * epsilon);
+      tuple2.setS(tuple1.getS() - 0.1 * epsilon);
+      assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+
+      tuple2.setX(tuple1.getX() - 0.1 * epsilon);
+      tuple2.setY(tuple1.getY() - 0.1 * epsilon);
+      tuple2.setZ(tuple1.getZ() - 0.1 * epsilon);
+      tuple2.setS(tuple1.getS() - 1.1 * epsilon);
       assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
 
       tuple2.setX(tuple1.getX() + 1.1 * epsilon);
       tuple2.setY(tuple1.getY() + 0.1 * epsilon);
+      tuple2.setZ(tuple1.getZ() + 0.1 * epsilon);
+      tuple2.setS(tuple1.getS() + 0.1 * epsilon);
       assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
 
       tuple2.setX(tuple1.getX() + 0.1 * epsilon);
       tuple2.setY(tuple1.getY() + 1.1 * epsilon);
+      tuple2.setZ(tuple1.getZ() + 0.1 * epsilon);
+      tuple2.setS(tuple1.getS() + 0.1 * epsilon);
+      assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+
+      tuple2.setX(tuple1.getX() + 0.1 * epsilon);
+      tuple2.setY(tuple1.getY() + 0.1 * epsilon);
+      tuple2.setZ(tuple1.getZ() + 1.1 * epsilon);
+      tuple2.setS(tuple1.getS() + 0.1 * epsilon);
+      assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
+      
+      tuple2.setX(tuple1.getX() + 0.1 * epsilon);
+      tuple2.setY(tuple1.getY() + 0.1 * epsilon);
+      tuple2.setZ(tuple1.getZ() + 0.1 * epsilon);
+      tuple2.setS(tuple1.getS() + 1.1 * epsilon);
       assertFalse(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
 
       tuple2.setX(tuple1.getX() + epsilon);
       tuple2.setY(tuple1.getY() + epsilon);
+      tuple2.setZ(tuple1.getZ() + epsilon);
+      tuple2.setS(tuple1.getZ() + epsilon);
       assertTrue(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
 
       tuple2.setX(tuple1.getX() - epsilon);
       tuple2.setY(tuple1.getY() - epsilon);
+      tuple2.setZ(tuple1.getZ() - epsilon);
+      tuple2.setS(tuple1.getS() - epsilon);
       assertTrue(TupleTools.epsilonEquals(tuple1, tuple2, epsilon));
    }
 
