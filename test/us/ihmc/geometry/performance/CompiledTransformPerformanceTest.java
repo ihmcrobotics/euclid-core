f65e39ca9700b9e04954be3514826cfe8224f0dd
Duncan Calvert <dcalvert@ihmc.us>
2017-02-09 10:39:59 -0600

    Re-commit compiled transform test.

---
+++ test/us/ihmc/geometry/performance/CompiledTransformPerformanceTest.java
@@ -0,0 +1,63 @@
+package us.ihmc.geometry.performance;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import us.ihmc.geometry.transform.RigidBodyTransform;
+import us.ihmc.geometry.tuple3D.Vector3D;
+
+/**
+ * This test is designed to measure the speed of transform code
+ * after it has been compiled by the JVM.
+ * 
+ * @author Duncan Calvert <a href="mailto:dcalvert@ihmc.us">(dcalvert@ihmc.us)</a>
+ */
+public class CompiledTransformPerformanceTest
+{
+   @Test
+   public void testTransformingAVectorManyTimes()
+   {
+      Random random = new Random(1230930210L);
+
+      RigidBodyTransform rigidBodyTransform = new RigidBodyTransform();
+      rigidBodyTransform.setRotationYawPitchRollAndZeroTranslation(2.0 * Math.PI * random.nextDouble(), 2.0 * Math.PI * random.nextDouble(),
+                                                                   2.0 * Math.PI * random.nextDouble());
+      rigidBodyTransform.setTranslation(10000.0 * (random.nextDouble() - 0.5), 10000.0 * (random.nextDouble() - 0.5), 10000.0 * (random.nextDouble() - 0.5));
+      
+      Vector3D vector = new Vector3D();
+      vector.setX(10000.0 * (random.nextDouble() - 0.5));
+      vector.setY(10000.0 * (random.nextDouble() - 0.5));
+      vector.setZ(10000.0 * (random.nextDouble() - 0.5));
+      
+      int numberOfMeasuredRuns = 20;
+      
+      String[] vectorOutput = new String[numberOfMeasuredRuns];
+      
+      for (int i = 0; i < numberOfMeasuredRuns; i++)
+      {
+         long nanoTime = tranformVectorNTimes(rigidBodyTransform, vector, 10000);
+         vectorOutput[i] = vector.toString();
+         System.out.println(i + ", " + nanoTime + ", ");
+      }
+      
+      for (int i = 0; i < vectorOutput.length; i++)
+      {
+         System.out.println("Vector: " + vectorOutput[i]);
+      }
+   }
+   
+   private long tranformVectorNTimes(RigidBodyTransform rigidBodyTransform, Vector3D vector, int numberOfOperationsPerMeasurement)
+   {
+      long start = System.nanoTime();
+      
+      for (int i = 0; i < numberOfOperationsPerMeasurement; i++)
+      {
+         rigidBodyTransform.transform(vector);
+      }
+      
+      long end = System.nanoTime();
+      
+      return end - start;
+   }
+}
