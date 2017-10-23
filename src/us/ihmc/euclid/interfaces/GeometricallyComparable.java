a0f79e53ab91ea54a30152e85baa2ddd23b744eb
Sylvain Bertrand <sbertrand@ihmc.us>
2017-10-22 21:24:37 -0500

    Extracted geometricallyEquals to its interface.

---
+++ src/us/ihmc/euclid/interfaces/GeometricallyComparable.java
@@ -0,0 +1,18 @@
+package us.ihmc.euclid.interfaces;
+
+public interface GeometricallyComparable<T extends GeometricallyComparable<T>>
+{
+   /**
+    * Tests if {@code this} and {@code other} represent the same geometry to an {@code epsilon}.
+    * <p>
+    * The implementation of this test depends on the type of geometry. For instance, two points will
+    * be considered geometrically equal if they are at a distance from each other that is less or
+    * equal than {@code epsilon}.
+    * </p>
+    *
+    * @param other the other geometry object to compare against this. Not modified.
+    * @param epsilon tolerance to use when comparing the two objects, usually refers to a distance.
+    * @return {@code true} if the two objects represent the same geometry, {@code false} otherwise.
+    */
+   boolean geometricallyEquals(T other, double epsilon);
+}
--- src/us/ihmc/euclid/interfaces/GeometryObject.java
+++ src/us/ihmc/euclid/interfaces/GeometryObject.java
@@ -8,24 +8,6 @@ package us.ihmc.euclid.interfaces;
  *
  * @param <T> the final type of the implementation of this interface.
  */
-public interface GeometryObject<T extends GeometryObject<T>> extends Transformable, EpsilonComparable<T>, Settable<T>, Clearable
+public interface GeometryObject<T extends GeometryObject<T>> extends Transformable, EpsilonComparable<T>, Settable<T>, Clearable, GeometricallyComparable<T>
 {
-   /**
-    * Tests if {@code this} and {@code other} represent the same geometry object to an
-    * {@code epsilon}.
-    * <p>
-    * The implementation of this test depends on the type of geometry. For instance, two points will
-    * be considered geometrically equal if they are at a distance from each other that is less or
-    * equal than {@code epsilon}.
-    * </p>
-    * <p>
-    * Note that {@code this.geometricallyEquals(other, epsilon) == true} does not necessarily imply
-    * {@code this.epsilonEquals(other, epsilon)} and vice versa.
-    * </p>
-    *
-    * @param other the other geometry object to compare against this. Not modified.
-    * @param epsilon tolerance to use when comparing the two objects, usually refers to a distance.
-    * @return {@code true} if the two objects represent the same geometry, {@code false} otherwise.
-    */
-   boolean geometricallyEquals(T other, double epsilon);
 }
