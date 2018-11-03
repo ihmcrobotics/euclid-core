71d05d01a8e9ecd946da25aa23abc4b26c50f6f2
Sylvain Bertrand <sbertrand@ihmc.us>
2018-11-03 14:20:59 -0500

    More tests.

--- test/us/ihmc/euclid/yawPitchRoll/YawPitchRollTest.java
+++ test/us/ihmc/euclid/yawPitchRoll/YawPitchRollTest.java
@@ -8,9 +8,98 @@ import org.junit.Test;
 
 import us.ihmc.euclid.orientation.interfaces.Orientation3DReadOnly;
 import us.ihmc.euclid.tools.EuclidCoreRandomTools;
+import us.ihmc.euclid.tools.EuclidCoreTestTools;
+import us.ihmc.euclid.tuple4D.Quaternion;
 
 public class YawPitchRollTest extends YawPitchRollBasicsTest<YawPitchRoll>
 {
+   @Test
+   public void testConstructors() throws Exception
+   {
+      Random random = new Random(423534);
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         YawPitchRoll ypr = new YawPitchRoll();
+         assertTrue(ypr.getYaw() == 0.0);
+         assertTrue(ypr.getPitch() == 0.0);
+         assertTrue(ypr.getRoll() == 0.0);
+      }
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         Quaternion q = EuclidCoreRandomTools.nextQuaternion(random);
+         YawPitchRoll ypr = new YawPitchRoll(q);
+         assertEquals(ypr.getYaw(), q.getYaw(), getEpsilon());
+         assertEquals(ypr.getPitch(), q.getPitch(), getEpsilon());
+         assertEquals(ypr.getRoll(), q.getRoll(), getEpsilon());
+      }
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         double yaw = random.nextDouble();
+         double pitch = random.nextDouble();
+         double roll = random.nextDouble();
+         YawPitchRoll ypr = new YawPitchRoll(yaw, pitch, roll);
+         assertTrue(ypr.getYaw() == yaw);
+         assertTrue(ypr.getPitch() == pitch);
+         assertTrue(ypr.getRoll() == roll);
+      }
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         double[] yprArray = {random.nextDouble(), random.nextDouble(), random.nextDouble(), random.nextDouble(), random.nextDouble(), random.nextDouble()};
+         YawPitchRoll ypr = new YawPitchRoll(yprArray);
+         assertTrue(ypr.getYaw() == yprArray[0]);
+         assertTrue(ypr.getPitch() == yprArray[1]);
+         assertTrue(ypr.getRoll() == yprArray[2]);
+      }
+   }
+
+   @Test
+   public void testSet()
+   {
+      super.testSet();
+
+      Random random = new Random(356);
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         YawPitchRoll ypr1 = EuclidCoreRandomTools.nextYawPitchRoll(random);
+         YawPitchRoll ypr2 = new YawPitchRoll();
+
+         assertFalse(ypr1.epsilonEquals(ypr2, getEpsilon()));
+         ypr2.set(ypr1);
+         assertTrue(ypr1.epsilonEquals(ypr2, getEpsilon()));
+         EuclidCoreTestTools.assertYawPitchRollEquals(ypr1, ypr2, getEpsilon());
+      }
+   }
+
+   @Test
+   public void testHashCode() throws Exception
+   {
+      Random random = new Random(621541L);
+      YawPitchRoll ypr = new YawPitchRoll();
+
+      ypr.setYaw(random.nextDouble());
+      ypr.setPitch(random.nextDouble());
+      ypr.setRoll(random.nextDouble());
+
+      int newHashCode, previousHashCode;
+      newHashCode = ypr.hashCode();
+      assertEquals(newHashCode, ypr.hashCode());
+
+      previousHashCode = ypr.hashCode();
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         ypr.setElement(i % 3, random.nextDouble());
+         newHashCode = ypr.hashCode();
+         assertNotEquals(newHashCode, previousHashCode);
+         previousHashCode = newHashCode;
+      }
+   }
+
    @Override
    public YawPitchRoll createEmptyYawPitchRoll()
    {
